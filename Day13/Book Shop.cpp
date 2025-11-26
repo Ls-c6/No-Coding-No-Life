99359548c1dda216d54fe9df26e3e50d89b2d325
Ls-c6 <150994905+Ls-c6@users.noreply.github.com>
2025-11-26 13:07:38 +0800

    init

--- Day13/Book Shop.cpp
+++ Day13/Book Shop.cpp
@@ -8,7 +8,6 @@ int main()
     int n,x;
     scanf("%d%d",&n,&x);
     vector<int> price(n),page(n),dp(x+1,0);
-    dp[0]=0;
     for (int i=0;i<n;++i) scanf("%d",&price[i]);
     for (int i=0;i<n;++i) scanf("%d",&page[i]);
     for (int i=0;i<n;++i) for (int j=x;j>0;--j) if (j-price[i]>=0) dp[j]=max(dp[j],dp[j-price[i]]+page[i]);
